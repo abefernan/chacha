b3cde5efa1e168a1c9bf6cede7a696c7c7093522
abefernan <44572727+abefernan@users.noreply.github.com>
2024-08-05 23:42:23 +0200

    Add mocks

---
+++ src/lib/mocks/mockUsers.ts
@@ -0,0 +1,26 @@
+import { User } from "@/types/user";
+
+export const mockUsers: Readonly<Array<User>> = [
+  {
+    address: "0xa575d7ca2a9ae1cb6c2d780fd718d3552d9a6528",
+    name: "satoshint",
+    pfpUrl: "https://github.com/shadcn.png",
+  },
+  {
+    address: "0x1231231231231231231231231231231231231231",
+    name: "mark",
+    pfpUrl: "https://github.com/shadcn.png",
+  },
+
+  {
+    address: "0x7897897878978978789789787897897878978978",
+    name: "mary",
+    pfpUrl: "https://github.com/shadcn.png",
+  },
+
+  {
+    address: "0x3475353475353475353475353475353475353475",
+    name: "maurice",
+    pfpUrl: "https://github.com/shadcn.png",
+  },
+];
